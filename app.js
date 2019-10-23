55245c86da8c8a65f8a46b30585e26c99bf9a5bd
Luwengier <g217881111@gmail.com>
2019-10-23 18:34:36 +0800

    Add shorten and home route file in routes folder

--- app.js
+++ app.js
@@ -28,30 +28,8 @@ db.once('open', () => {
 
 
 // setting routes
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/', (req, res) => {
-  const inputUrl = req.body.inputUrl
-  const newUrl = 'http://localhost:3000/' + Math.random().toString(36).slice(-5)
-  const shorten = new Shorten({
-    inputUrl: inputUrl,
-    newUrl: newUrl
-  })
-  shorten.save(err => {
-    if (err) return console.error(err)
-    return res.render('result', { newUrl })
-  })
-})
-
-app.get('/:newUrl', (req, res) => {
-  Shorten.findOne({ newUrl: ('http://localhost:3000/' + req.params.newUrl) }, (err, shorten) => {
-    if (err) console.error(err)
-    res.redirect(`${shorten.inputUrl}`)
-  })
-})
-
+app.use('/', require('./routes/home'))
+app.use('/', require('./routes/shorten'))
 
 app.listen(3000, () => {
   console.log('Server is listen to port 3000')
---
+++ routes/shorten.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const Shorten = require('../models/shorten')
+
+
+router.post('/', (req, res) => {
+  const inputUrl = req.body.inputUrl
+
+  let newUrls = new Promise((resolve, reject) => {
+    Shorten.find({}, (err, shortens) => {
+      let newUrlArr = []
+      shortens.forEach(shorten => {
+        newUrlArr.push(shorten.newUrl)
+      })
+      return resolve(newUrlArr)
+    })
+  })
+
+  async function start() {
+    try {
+      let newUrlArr = await newUrls
+      let newUrl = 'http://localhost:3000/' + Math.random().toString(36).slice(-5)
+      while (newUrlArr.includes(newUrl)) {
+        newUrl = 'http://localhost:3000/' + Math.random().toString(36).slice(-5)
+      }
+      const newShorten = new Shorten({
+        inputUrl: inputUrl,
+        newUrl: newUrl
+      })
+
+      newShorten.save(err => {
+        if (err) return console.error(err)
+        return res.render('result', { newUrl })
+      })
+    } catch (e) {
+      console.warn(e)
+    }
+  }
+  start()
+})
+
+router.get('/:newUrl', (req, res) => {
+  Shorten.findOne({ newUrl: ('http://localhost:3000/' + req.params.newUrl) }, (err, shorten) => {
+    if (err) console.error(err)
+    res.redirect(`${shorten.inputUrl}`)
+  })
+})
+
+
+module.exports = router
